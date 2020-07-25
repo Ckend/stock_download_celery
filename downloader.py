e60871f3ee1ad265e41fb73f102494e7156bc112
Shimeng <zhaoshimeng@gmail.com>
2020-07-25 13:39:11 +0200

    添加get_segment_list函数：每次向tushare请求100个code（tushare单次request包含code数的上限）的结果，使总的request个数降低（3601个code可以通过37个request完成），进而不会到达tushare每分钟800次请求的上限

--- downloader.py
+++ downloader.py
@@ -2,6 +2,8 @@
 # https://pythondict.com
 
 from tasks import get_stock_daily
+from tushare_utils import get_segment_list
+
 
 def delay_stock_data(start_date, end_date):
     """
@@ -13,11 +15,12 @@ def delay_stock_data(start_date, end_date):
     """
 
     codes = open('./codes.csv', 'r', encoding='utf-8').readlines()
+    segment_list = get_segment_list(codes_from_csv=codes)
 
     # 遍历所有股票ID
-    for code in codes:
+    for code_segment in segment_list:
         get_stock_daily.apply_async(
-            (start_date, end_date, code), retry=True
+            (start_date, end_date, code_segment), retry=True
         )
 
 delay_stock_data("20180101", "20200725")
